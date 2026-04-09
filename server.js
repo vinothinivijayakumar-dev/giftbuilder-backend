735eae4dcaf62ec3bb5d1d4d66836d40ea0ef819
Vinothini Vijayakumar <vinothinivijayakumar123@gmail.com>
2026-04-09 10:36:00 +0530

    backend

--- server.js
+++ server.js
@@ -18,3 +18,7 @@ app.use("/api/auth", authRoutes);
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
 });
+
+app.get("/", (req, res) => {
+  res.send("GiftBuilder Backend is Running");
+});
